0db1d0f13553b0117a111d0e0ff9cdc3c5fbe278
April Tsang <april@tofugear.com>
2013-09-02 15:45:39 +0800

    handle nil argument for Customer.new

--- lib/fastspring-saasy/customer.rb
+++ lib/fastspring-saasy/customer.rb
@@ -3,11 +3,13 @@ module FastSpring
     attr_accessor :first_name, :last_name, :company, :email, :phone_number
 
     def initialize(details)
-      @first_name = details.fetch('firstName', '')
-      @last_name = details.fetch('lastName', '') 
-      @company = details.fetch('company', '') 
-      @email = details.fetch('email')
-      @phone_number = details.fetch('phoneNumber', '')
+      unless details.nil?
+        @first_name = details.fetch('firstName', '')
+        @last_name = details.fetch('lastName', '')
+        @company = details.fetch('company', '')
+        @email = details.fetch('email')
+        @phone_number = details.fetch('phoneNumber', '')
+      end
     end
 
   end
